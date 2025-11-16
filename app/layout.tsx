6c6c12b10ac48a74e1eb0d85c4db2bf538471801
vophong90 <141991252+vophong90@users.noreply.github.com>
2025-11-16 18:08:12 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,25 @@
-import './globals.css';
-export const metadata = {
-  title: 'Clinical Skills Delphi',
-  description: 'MVP khảo sát kỹ năng lâm sàng',
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Clinical Skills Delphi",
+  description: "MVP khảo sát kỹ năng lâm sàng",
+  icons: {
+    icon: [
+      {
+        url: "/favicon-16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="vi">
       <body style={{ fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial' }}>
-        <div style={{ maxWidth: 980, margin: '0 auto', padding: 16 }}>
+        <div style={{ maxWidth: 980, margin: "0 auto", padding: 16 }}>
           {children}
         </div>
       </body>
